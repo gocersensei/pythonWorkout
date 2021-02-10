2644fa1bc0468223eab022ce95889bf5499c82e5
gocersensei <gocersensei@gmail.com>
2021-02-10 14:53:32 +0000

    initial commit

---
+++ ubbidubbi.py
@@ -0,0 +1,15 @@
+def ubbi_dubbi(word):
+    output = []
+    for letter in word:
+        if letter in 'aeiou':
+            output.append(f'ub{letter}')
+            # Why append to a list, and not to
+            # a string? To avoid allocating too
+            # much memory. For short strings,
+            # it’s not a big deal. But for long
+            # loops and large strings, it’s a
+            # bad idea.
+        else:
+            output.append(letter)
+    return ''.join(output)
+print(ubbi_dubbi('python'))
